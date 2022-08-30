9365f0739bcddae3a43bd7400ec28f5c374de61e
Maahes <ionutngp@gmail.com>
2022-08-30 14:43:11 +0300

    Declare a function that plays one round

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function getPlayerChoice() {
     }
     else return 'scissors';
 }
+
+function playRound(computerSelection, playerSelection) {
+    if(computerSelection === playerSelection) {
+        return 'It\'s a tie!';
+    }
+    else {
+        if(computerSelection === 'rock' && playerSelection === 'paper')
+        return 'You Win! Paper beats Rock,';
+        else if(computerSelection === 'rock' && playerSelection === 'scissors')
+        return 'You Lose! Rock beats Scissors.';
+        else if(computerSelection === 'paper' && playerSelection === 'rock')
+        return 'You Lose! Paper beats Rock.';
+        else if(computerSelection === 'paper' && playerSelection === 'scissors')
+        return 'You Win! Scissors beats Paper.';
+        else if(computerSelection === 'scissors' && playerSelection === 'rock')
+        return 'You Win! Rock beats Scissors.';
+        else if(computerSelection === 'scissors' && playerSelection === 'paper')
+        return 'You Lose! Scissors beats Paper.';
+    }
+}
+
+console.log(playRound(getComputerChoice(),getPlayerChoice()));
