341b87de545eafef25b2495fc74e189a05e16490
Maahes <ionutngp@gmail.com>
2022-08-30 14:40:57 +0300

    Declare player's choice function

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choice[randomNumber].toLowerCase();
 }
+
+function getPlayerChoice() {
+    const choice = prompt("What do you choose?");
+    if (choice.toLowerCase() === 'rock') {
+        return 'rock';
+    }
+    else if (choice.toLowerCase() === 'paper') {
+        return 'paper';
+    }
+    else return 'scissors';
+}
