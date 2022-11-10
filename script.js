e38615ee78b403029efffd7347fcc47363615b3a
maahesi <ionutngp@gmail.com>
2022-11-10 15:24:45 +0200

    Add functionality to play 5 rounds

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let playerScore = 0,
-    computerScore = 0;
+    computerScore = 0,
+    counter = 0;
 
 const resultsDiv = document.createElement('div');
+resultsDiv.setAttribute('style','white-space: pre;');
 const results = document.querySelector('.results');
 
 function getComputerChoice() {
@@ -22,47 +24,50 @@ function getPlayerChoice(choice) {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        playerScore++;
-        computerScore++;
-        resultsDiv.textContent = ('It\'s a tie!');
+function playGame(playerSelection, computerSelection) {
+    if (counter < 5) {
+        if (playerSelection == computerSelection) {
+            playerScore++;
+            computerScore++;
+            resultsDiv.textContent = 'It\'s a tie! ';
+        }
+        else if (
+            (playerSelection === 'rock' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' && computerSelection === 'rock')
+        ) {
+            computerScore++;
+            resultsDiv.textContent = `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}. `;
+        }
+        else if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'rock')
+        ) {
+            playerScore++;
+            resultsDiv.textContent = `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}. `;
+        }
+        resultsDiv.textContent += `\r\nYour Score: ${playerScore} - Computer Score: ${computerScore}`;
+    } 
+    counter++;
+    if (counter == 5) {
+        if (playerScore > computerScore) {
+            resultsDiv.textContent += '\r\nYou won the game!!!';
+        } else if (playerScore < computerScore) {
+            resultsDiv.textContent += '\r\nThe computer won the game!!!';
+        } else {
+            resultsDiv.textContent += '\r\nThe game ended in a tie!!!';
+        }
     }
-    else if (
-        (playerSelection === 'rock' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'rock')
-    ) {
-        computerScore++;
-        resultsDiv.textContent = (`You lose! ${computerSelection} beats ${playerSelection}`);
-    }
-    else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'rock')
-    ) {
-        playerScore++;
-        resultsDiv.textContent = (`You win! ${playerSelection} beats ${computerSelection}`);
+    if (counter > 5) {
+        window.location.reload();
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let decision = playRound(getPlayerChoice(),getComputerChoice());
-        console.log(decision);
-        console.log(`Your Score: ${playerScore} - Computer's score: ${computerScore}`);
-    }
-
-    if (playerScore > computerScore) {
-        console.log('You won the game!');
-    } else if (playerScore < computerScore) {
-        console.log('The computer won the game!');
-    } else {
-        console.log('The game ended in a tie!');
-    }
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', () => playRound(getPlayerChoice(button.className),getComputerChoice())));
-
+buttons.forEach(button => button.addEventListener('click', () => playGame(getPlayerChoice(button.className),getComputerChoice())));
 results.appendChild(resultsDiv);
